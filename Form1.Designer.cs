f1e5b3e2896ec937c53cdceb8489266a13fb8416
hodayachaya <hodaya4733@gmail.com>
2022-03-22 17:36:51 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace emgucv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(59, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 352);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 466);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace emgucv
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap image;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        string path = @"E:\hodaya\project\Digivert\ex1.png";
+        //        Image<Bgr, byte> image = new Image<Bgr, byte>(path);
+        //        pictureBox1.Image = image.ToBitmap();
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, Filter = "JPEG,*.jpg" })
+            { //לפתוח חלון של בחירת קובץ
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                   image = new Bitmap(ofd.FileName);
+                   pictureBox1.Image = image;
+               
+                }
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        Color color = image.GetPixel(x, y);
+                        var ava = (color.R + color.B + color.G) / 3;
+
+
+
+                        // image.SetPixel(x,y,Color.FromArgb(color.A,ava,ava,ava));
+                        int temp;
+                        if (ava > 128)
+                            temp = 255;
+
+                        else
+                        {
+                            temp = 0;
+                        }
+
+                        image.SetPixel(x, y, Color.FromArgb(color.A, temp, temp, temp));
+
+
+                    }
+                }
+               
+               
+
+            }
+        }
+        public void changecolor()
+        {
+
+            //Bitmap BlackAndWhite = new Bitmap(@"C:\Users\נטלי\Desktop\תוכנות הודיה\Digivert-master\Digivert\ex1.JPG ");
+            //var pathimage = @"C:\Users\נטלי\Desktop\תוכנות הודיה\Digivert - master\Digivert\ex1.JPG ";
+            ////var i = Image.FromFile(pathimage);
+
+
+            
+            //image.Save(newPath, ImageFormat.Png);
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
